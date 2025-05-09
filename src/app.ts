8f27b18c965fe4c0c09f1a4cbb39e3192e72585b
Md. Asif Shahariar <asshah419@gmail.com>
2025-05-09 15:01:18 +0300

    gitignore file added

--- src/app.ts
+++ src/app.ts
@@ -9,11 +9,11 @@ const app: Application = express();
 
 //parsers
 app.use(express.json());
-app.use(cors({ origin: ['http://localhost:5173'] }));
+app.use(cors({ origin: ['http://localhost:5173/api/v1'] }));
 app.use(cookieParser());
 
 // application routes
-app.use('/api', router);
+app.use('/api/v1', router);
 
 app.get('/', (req: Request, res: Response) => {
   res.send("hello it's working");
